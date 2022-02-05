c3a97689a38184430947ccedc124ffc48b82e832
San San Thi <sansanthi1996@gmail.com>
2022-02-05 14:49:55 +0630

    modified app.js

--- app.js
+++ app.js
@@ -2,30 +2,22 @@ let monthlyPrice = [19.99, 24.99, 39.99];
 let yearlyPrice = [199.99, 249.99, 399.99];
 let priceTag = document.querySelectorAll('.price');
 const toggleBtn = document.querySelector('.btn-toggle');
-let i = 0;
 
-priceTag.forEach(function (price) {
-    console.log('yearly')
-    console.log('i value ', i);
-    price.innerHTML = yearlyPrice[i++];
-});
+showPrice(yearlyPrice);
+
 toggleBtn.addEventListener('click', function (e) {
     e.preventDefault();
     let toggleAction = toggleBtn.children[0];
     toggleAction.classList.toggle('off');
-    //console.log(toggleAction.classList);
     if (toggleAction.classList.contains('off')) {
-        i = 0;
-        priceTag.forEach(function (price) {
-            console.log('monthly')
-            price.innerHTML = monthlyPrice[i++];
-        });
-    } else {
-        i = 0;
-        priceTag.forEach(function (price) {
-            console.log('yearly')
-            console.log('i value ', i);
-            price.innerHTML = yearlyPrice[i++];
-        });
+       
+        showPrice(monthlyPrice);
+    } else {        
+       showPrice(yearlyPrice);
     }
 })
+function showPrice(item){
+    priceTag.forEach(function (price, index) {
+        price.innerHTML = item[index];
+    });
+}
